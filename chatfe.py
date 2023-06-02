b9390c3bb5a440214a0161fcecf306cef61ef15c
Cecil Coupe <ccoupe@local>
2023-06-02 14:02:28 -0600

    keep context, more threads - 4 was what it was using.

--- chatfe.py
+++ chatfe.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, send_file, abort, jsonify
-from gpt4allj import Model
 import sys
 
+from gpt4allj import Model
 #model = Model("/mnt/models/gpt4all-ui/models/gpt_4all/ggml-gpt4all-j-v1.3-groovy.bin")
 model = Model("models/gpt_4all/ggml-gpt4all-j-v1.3-groovy.bin")
-
+'''
+# try native lib version
+from gpt4allj import Model, load_library
+lib = load_library('/app/lib/libgptj.so', '/app/lib/libggml.so')
+model = Model("models/gpt_4all/ggml-gpt4all-j-v1.3-groovy.bin", lib=lib)
+'''
 if __name__ == "__main__":
   
   app = Flask(__name__)
@@ -12,7 +17,9 @@ if __name__ == "__main__":
   @app.route('/',methods=['POST'])
   def get_prompt(): 
     content = request.json
-    result = model.generate(content['prompt'])
+    result = model.generate(content['prompt'],
+        n_thread=8,
+        reset=FALSE)
     print(result)
     return jsonify({'reply': result})
 
